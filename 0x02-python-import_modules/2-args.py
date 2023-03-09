c032965e2d8e8202eaec2b10176fda3c9c8d0fc5
Alejio20 <humbleotamiri@gmail.com>
2023-03-09 11:51:28 -0800

    Added args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,9 @@ if __name__ == "__main__":
         print('0 arguments.')
     elif len(arg) == 2:
         print('1 argument:')
-        print('1: {:s}'.format(arg[1]))
     elif len(arg) > 2:
         print('{:d} arguments:'.format(len(arg)))
-        i = 1
-        for n in arg:
-            print('{:d}: {:s}'.format(i, arg[i]))
-            i++
+    i = 1
+    for n in arg:
+        print('{:d}: {:s}'.format(i, arg[i]))
+        i += 1
