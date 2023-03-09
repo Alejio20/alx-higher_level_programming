317fa2b4463d39085b2aae28882448f8b10d1eb8
Alejio20 <humbleotamiri@gmail.com>
2023-03-09 13:14:52 -0800

    Added hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # Prints all the names defined by the compiled module "hidden_4.pyc"
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
 
     modules = dir(hidden_4)
     for module in modules:
