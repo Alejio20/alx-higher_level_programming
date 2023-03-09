86ed7630ea168b7cfa3b8dfb7058d5bc2214b3e2
Alejio20 <humbleotamiri@gmail.com>
2023-03-09 11:38:12 -0800

    Added args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    import sys
+
+    arg = sys.argv
+    if len(arg) == 1:
+        print('0 arguments.')
+    elif len(arg) == 2:
+        print('1 argument:')
+        print('1: {:s}'.format(arg[1]))
+    elif len(arg) > 2:
+        print('{:d} arguments:'.format(len(arg)))
+        i = 1
+        for n in arg:
+            print('{:d}: {:s}'.format(i, arg[i]))
+            i++
