d5d319c794b43cdc3733826b81b923cdebbe9ee6
Alejio20 <humbleotamiri@gmail.com>
2023-03-13 13:43:29 -0700

    Added multiple_returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,9 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
+    if (sentence == 0):
+        first = "None"
+    else:
+        first = sentence[0]
 
     return (length, first)
