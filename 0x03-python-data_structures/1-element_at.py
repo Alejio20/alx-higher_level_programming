59b2754cc8df609a468f0361f90391a5b0e1cfc3
Alejio20 <humbleotamiri@gmail.com>
2023-03-13 02:26:40 -0700

    Added element_at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@
 def element_at(my_list, idx):
     if idx < 0:
         return ("None")
-    elif idx - 1 > len(my_list):
+    elif idx >= len(my_list):
         return ("None")
     return my_list[idx]
