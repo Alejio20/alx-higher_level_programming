180e1e41004c3aa4934ae8a0032f892bbf7b1fb8
Alejio20 <humbleotamiri@gmail.com>
2023-03-15 12:04:06 -0700

    Added square_matrix_simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for m in matrix:
+        new_matrix1 = []
+        for n in m:
+            new_matrix1.append(n ** 2)
+        new_matrix.append(new_matrix1)
+    return new_matrix
