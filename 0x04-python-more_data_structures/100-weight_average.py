913f6ec524d92c836bfa5f869dd42cfd56cffc0e
Alejio20 <humbleotamiri@gmail.com>
2023-03-15 17:37:20 -0700

    Added weight_average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    tuple_sum = 0
-    weight_sum = 0
-    for score, weight in my_list:
-        tuple_sum += (score * weight)
-        weight_sum += weight
+    if (my_list):
+        tuple_sum = 0
+        weight_sum = 0
+        for score, weight in my_list:
+            tuple_sum += (score * weight)
+            weight_sum += weight
 
-    return (tuple_sum / weight_sum)
+        return (tuple_sum / weight_sum)
+    else:
+        return 0
