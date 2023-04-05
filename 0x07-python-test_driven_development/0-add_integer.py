40f275a4839c5481d35a29189684d907b0212692
Alejio20 <humbleotamiri@gmail.com>
2023-04-05 14:58:34 -0700

    Added tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module has only the add_integer method
+"""
+def add_integer(a, b=98):
+    """
+    Returns an integer of the addition of a and b which can be int or float
+
+    Arguments:
+        a: first integer
+        b: second integer with default value of 98
+
+    Return:
+        An integer addition of a and b
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
