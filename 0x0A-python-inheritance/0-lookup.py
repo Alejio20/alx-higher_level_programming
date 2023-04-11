6973cf1cab738d2ce9e8d77a45a0a54a16fe09c5
Alejio20 <humbleotamiri@gmail.com>
2023-04-11 04:15:06 -0700

    Added lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module definition for lookup moethod"""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+
+    return dir(obj)
