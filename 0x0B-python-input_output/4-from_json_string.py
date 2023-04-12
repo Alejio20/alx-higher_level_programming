506f5e4bc4cfeb0c063f9fc2ddf6ce3b0b55f348
Alejio20 <humbleotamiri@gmail.com>
2023-04-12 11:33:33 -0700

    Added from_json_string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,6 +4,7 @@ import json
 
 
 def from_json_string(my_obj):
-    """returns an object (Python data structure) represented by a JSON string"""
+    """returns an object (Python data structure)
+    represented by a JSON string"""
 
     return json.loads(my_obj)
