cc196fc32942a4ddb375b805b7c9028dfba27c11
Alejio20 <humbleotamiri@gmail.com>
2023-04-12 11:08:30 -0700

    Added write_file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""write_file definition"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written"""
+
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return file.write(text)
