083c1c7824cf0aa6440f13980bcbafa30bfcf367
Alejio20 <humbleotamiri@gmail.com>
2023-04-12 19:04:42 -0700

    Added student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A Student class"""
+
+
+class Student:
+    """Student class definition"""
+
+    def __init__(self, first_name, last_name, age):
+        """Student initialization"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+
+        return self.__dict_
