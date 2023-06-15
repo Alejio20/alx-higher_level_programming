11f4fde65d289d51d6f7879023d915984eb61538
Alejio20 <humbleotamiri@gmail.com>
2023-06-15 10:49:38 -0700

    Added multi_languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const myVar = "C is fun\nPython is cool\nJavaScript is ammazing"
-console.log(myVar)
+const myVar = "C is fun\nPython is cool\nJavaScript is ammazing";
+console.log(myVar);
